327ed9263dd0e0eebcd454506b0b8f340746d0ae
Luis Grajales <49939259+LuisGrajales@users.noreply.github.com>
2020-11-04 23:09:30 -0600

    Update googleAcademic.py

--- googleAcademic.py
+++ googleAcademic.py
@@ -24,7 +24,8 @@ def findGoogle(search_param):
     chrome_options.add_argument("--headless")
 
     #Chrome drivers
-    driver = webdriver.Chrome("../../usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
+    chrome_path = os.path.abspath("../../usr/lib/chromium-browser/chromedriver")
+    driver = webdriver.Chrome(chrome_path, chrome_options=chrome_options)
 
     #Navegar a google academico
     driver.get('https://scholar.google.com/citations?view_op=search_authors&mauthors=&hl=en&oi=ao')
