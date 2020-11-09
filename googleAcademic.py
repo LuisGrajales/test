f1da3784a4c0b0d43153b296d54021413d3d20b3
LuisGrajales <luis.logs1@gmail.com>
2020-11-08 20:37:50 -0600

    sd

--- googleAcademic.py
+++ googleAcademic.py
@@ -39,7 +39,7 @@ def findGoogle(search_param):
         WebDriverWait(driver, timeout = 30).until(lambda d : d.find_elements_by_class_name("gsc_1usr"))
         print("se encontraton resultado")
     except:
-        return [{ "error" : "timeout" }]
+        return [{ "error" : "No se encontraron resultados" }]
 
     #Entrar a los articulos 
     driver.find_element_by_class_name('gs_ai_pho').click()
