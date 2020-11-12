38d8ccd4f66da65880dec750d9378c6545431d58
LuisGrajales <luis.logs1@gmail.com>
2020-11-11 21:23:12 -0600

    sd

--- researchGate.py
+++ researchGate.py
@@ -295,7 +295,7 @@ def onlyHTML (search_param):
 	# message_bytes = base64.b64decode(base64_bytes)
 	# message = message_bytes.decode('ascii')
 
-	return message
+	return site
 
 def byPass (search_param):
 	warnings.filterwarnings("ignore", category=DeprecationWarning) 
