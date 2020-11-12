ab10c2cb578766c0f030646dd0525c244a09fa35
LuisGrajales <luis.logs1@gmail.com>
2020-11-11 19:59:29 -0600

    sd

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from researchGate import findResearchGate, findOnePage, findOnePageByClass, onlyHTML
+from researchGate import findResearchGate, findOnePage, findOnePageByClass, onlyHTML, byPass
 from googleAcademic import findGoogle
 from database import queryDatabase, insertData, insertTest, replaceTest
 from bson import json_util
@@ -65,7 +65,7 @@ def testinsertendpoint(name):
 def query_one_page_class(name):
 
     scrapeResults = {
-        'research_gate' : onlyHTML(name)
+        'research_gate' : byPass(name)
     }
 
     return scrapeResults
