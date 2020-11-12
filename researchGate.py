a389682f34a7e747dc0fd7680df3bdca3d9f9601
LuisGrajales <luis.logs1@gmail.com>
2020-11-11 21:37:12 -0600

    sd

--- researchGate.py
+++ researchGate.py
@@ -278,17 +278,18 @@ def onlyHTML (search_param):
 
 
 	# Navegar hacia el URL deseado con el nombre a buscar ya dentro del URI
-	url = "https://academic.microsoft.com/search?q=alexandro%20escudero%20nahon&qe=%40%40%40Composite(AA.AuN%3D%3D%27alexandro%20escudero%20nahon%27)&f=&orderBy=4&skip=0&take=10"
+	url = "https://academic.microsoft.com/home"
 	driver.get(url)
 	print("entro a la pagina")
 
-	# search = WebDriverWait(driver, timeout = 120).until(lambda d: d.find_element_by_id('search-input'))
-	# print("encotro input")
-	# param = "alexandro escudero-nahon"
-	# search.send_keys(param)
-	# print("escribio")
-	# search.send_keys(Keys.RETURN)
-	# print("mando la peticion")
+	search = WebDriverWait(driver, timeout = 120).until(lambda d: d.find_element_by_id('search-input'))
+	print("encotro input")
+	search.click()
+	param = "alexandro escudero-nahon"
+	search.send_keys(param)
+	print("escribio")
+	search.send_keys(Keys.RETURN)
+	print("mando la peticion")
 	title = driver.title
 	# base64_bytes = site.encode('ascii')
 	# message_bytes = base64.b64decode(base64_bytes)
