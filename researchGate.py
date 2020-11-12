e455fd3a4410c3d45929f06d9f9867f0e7045386
LuisGrajales <luis.logs1@gmail.com>
2020-11-11 18:12:00 -0600

    sd

--- researchGate.py
+++ researchGate.py
@@ -241,6 +241,8 @@ def onlyHTML (search_param):
 	chrome_options.add_argument('--ignore-ssl-errors')
 	chrome_options.add_argument("--disable-gpu")
 	chrome_options.add_argument("--no-sandbox")
+	chrome_options.add_argument("start-maximized")
+	chrome_options.add_argument("--disable-dev-shm-usage")
 	# chrome_options.add_argument("--headless")
 	# chrome_options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36")
 
@@ -250,7 +252,7 @@ def onlyHTML (search_param):
 	chrome_path = os.path.abspath("../../usr/lib/chromium-browser/chromedriver")
 	driver = webdriver.Chrome(chrome_path, chrome_options=chrome_options)
 
-	driver.maximize_window()
+	# driver.maximize_window()
 
 	# Navegar hacia el URL deseado con el nombre a buscar ya dentro del URI
 	driver.get('https://www.researchgate.net/search/publication?q="{}"'.format(search_param))
